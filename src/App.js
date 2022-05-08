a5621b2c8e35f853f51c807f31e3c61031c358c7
PallaviKatari <pallavikatari@gmail.com>
2022-05-08 23:05:34 +0530

    Commit_08.05.22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from 'react';  
+import Addstudent from './Student/Addstudent';  
+import Studentlist from './Student/Studentlist';  
+import Editstudent from './Student/Editstudent';  
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';  
+import './App.css';  
+function App() {  
+  return (  
+    <Router>  
+      <div className="container">  
+        <nav className="navbar navbar-expand-lg navheader">  
+          <div className="collapse navbar-collapse" >  
+            <ul className="navbar-nav mr-auto">  
+              <li className="nav-item">  
+                <Link to={'/Addstudent'} className="nav-link">Addstudent</Link>  
+              </li>  
+              <li className="nav-item">  
+                <Link to={'/Studentlist'} className="nav-link">Student List</Link>  
+              </li>  
+            </ul>  
+          </div>  
+        </nav> <br />  
+        <Routes>  
+          <Route exact path='/Addstudent' element={<Addstudent/>} />  
+          <Route path='/edit/:id' element={<Editstudent/>} />  
+          <Route path='/Studentlist' element={<Studentlist/>} />  
+        </Routes>  
+      </div>  
+    </Router>  
+  );  
+}  
+  
+export default App;  
